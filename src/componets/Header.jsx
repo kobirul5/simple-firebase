600592356f8b448befd738d05d35f3bcd24c4b7c
kobirul5 <kobirul05j@gmail.com>
2024-11-09 00:16:57 +0600

    google authitacation

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -2,9 +2,13 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className="mx-auto text-5xl">
-            <NavLink to="/" className="btn">Home</NavLink>
-            <NavLink to="login" className="btn">login</NavLink>
+        <div className=" flex justify-center mx-auto text-5xl">
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><NavLink to="/" className="">Home</NavLink></li>
+                    <li><NavLink to="login" className="">login</NavLink></li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -1,8 +1,52 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../firebase/firebase.init";
+
 
 const Login = () => {
+    const provider = new GoogleAuthProvider()
+    const hanleGoogleSignUp = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log("Error", error)
+            })
+    }
     return (
-        <div>
-            <button>Log in with google</button>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                            <button onClick={hanleGoogleSignUp} className="btn mt-5 bg-green-950 text-white">Login With google</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
